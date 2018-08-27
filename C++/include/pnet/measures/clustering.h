4cfc37e0ddfdb497b9f61987d1c514cb2ce392e6
Amin <amin-kaveh@hotmail.com>
2018-08-27 13:32:31 +0200

    Expected Clustering Coefficient is Done.

---
+++ C++/include/pnet/measures/clustering.h
@@ -0,0 +1,126 @@
+/**
+ * History:
+ * - 2018.08.16 file created.
+ */
+
+#ifndef UU_PNET_MEASURES_CLUSTERING_H_
+#define UU_PNET_MEASURES_CLUSTERING_H_
+
+#include <vector>
+#include <algorithm>
+
+#include "net/datastructures/objects/Vertex.h"
+#include "net/datastructures/objects/EdgeMode.h"
+#include "core/exceptions/assert_not_null.h"
+#include "core/utils/Counter.h"
+
+
+namespace uu {
+namespace net {
+
+/**
+ * Returns the expected clustering coefficient of a vertex in graph. This measure has been introduced in  (J. J. Pfeiffer and J. Neville. Methods to determine node centrality and clustering in graphs with uncertain structure. In 5th International AAAI Conference on Weblogs and Social Media, 2011).
+ * @param g input graph
+ * @param v input vertex
+ * @return the expected clustering coefficient of the vertex in g
+ */
+template<typename G>
+double                                                              
+expected_clustering_coefficient(
+               const G* g,
+	       const Vertex* v
+);
+
+
+/**
+ * Returns the distribution of the expected clustering coefficient in graph.
+ * @param g input graph
+ * @return the the distribution of the expected clustering coefficient in g
+ */
+/*
+template<typename G>
+std::vector<double>
+expected_clustering_coefficient_distribution(
+               const G* g,
+	       const Vertex* v,
+               //const EdgeMode mode                                             
+);
+*/
+
+
+
+
+/* Definitions */
+
+template<typename G>
+double                                                              
+expected_clustering_coefficient(
+               const G* g,
+	       const Vertex* v
+)
+{
+    using namespace std;
+    core::assert_not_null(g, "degree", "g");
+    core::assert_not_null(g, "degree", "v");
+
+    
+    // neighbors of v
+    auto nghb = g->edges()->neighbors(v,uu::net::EdgeMode::INOUT);
+    if(nghb->size()==0)
+    {
+        return 0;
+    }
+
+    // probability of edges incident to v.
+    vector<double> P;
+    for (auto edge: *g->edges()->incident(v,uu::net::EdgeMode::INOUT))
+    {
+        auto p = g->edges()->attr()->get_probability(edge);
+        if (!p.null)
+        {
+            P.push_back(p.value);
+        }
+    }
+
+    double denominator = 0;             // expected number of coexisting pairs
+    double nominator =0;                // expected number of triangles
+
+    for (unsigned int i = 0 ; i < nghb->size() ; i++)
+    {
+        auto n = g->edges()->neighbors(nghb->get_at_index(i),uu::net::EdgeMode::INOUT);
+        for (unsigned int j = i+1 ; j < nghb->size() ; j++)
+        {
+            denominator += P[i]*P[j];
+            auto pos = n->get_index(nghb->get_at_index(j));
+            //std::cout << "Position of nghb   " << j << "   in neighbors of nghb   " << i << "   is: " << pos << std::endl;
+            if (pos != -1)
+            {	
+                auto e = g->edges()->incident(nghb->get_at_index(i),uu::net::EdgeMode::INOUT);
+                auto p = g->edges()->attr()->get_probability(e->get_at_index(pos));
+                //std::cout << " probability is: " << p.value << std::endl; 
+                nominator += P[i]*P[j]*p.value;
+            }
+        }
+    }
+
+     std::cout << "nominator:    " << nominator << std::endl;
+     std::cout << "denominator:  " << denominator << std::endl;
+
+     return nominator/denominator;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+#endif
---
+++ C++/test/pnet/measures/clustering_test.cpp
@@ -0,0 +1,100 @@
+#include "gtest/gtest.h"
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "pnet/io/read_probabilistic_graph.h"
+#include "pnet/measures/clustering.h"
+
+
+
+class pnet_measures_clustering_test : public ::testing::Test
+{
+  protected:
+
+    std::string test_file_name = "net_io_read_graph_file.tmp";
+    std::unique_ptr<uu::net::ProbabilisticGraph> g;
+
+    void
+    SetUp() override
+    {
+        // Create a test file
+        std::ofstream test_file;
+        test_file.open(test_file_name);
+
+        if (!test_file.is_open())
+        {
+            FAIL()
+                    << "Could not create temporary file. Test not executed.";
+        }
+
+        test_file << "#VERSION           " << std::endl;
+        test_file << "2.0                " << std::endl;
+        test_file << "                   " << std::endl;
+        test_file << "#TYPE              " << std::endl;
+        test_file << "undirected         " << std::endl;
+        test_file << "probabilistic      " << std::endl;
+        test_file << "                   " << std::endl;
+        test_file << "#VERTICES          " << std::endl;
+        test_file << "v0                 " << std::endl;
+        test_file << "                   " << std::endl;
+        test_file << "#EDGES             " << std::endl;
+        test_file << "v1,v2,0.1           " << std::endl;
+        test_file << "v1,v3,0.1           " << std::endl;
+        test_file << "v2,v3,0.1           " << std::endl;
+        test_file << "v2,v4,0.1           " << std::endl;
+        test_file << "v2,v5,0.1           " << std::endl;
+        test_file << "v2,v6,0.1           " << std::endl;
+        test_file << "v3,v5,0.1           " << std::endl;
+        test_file << "v3,v6,0.1           " << std::endl;
+        test_file << "v4,v5,0.1           " << std::endl;
+        test_file << "v5,v6,0.1           " << std::endl;
+        test_file << "v5,v7,0.1           " << std::endl;
+        test_file << "v6,v7,0.1           " << std::endl;
+        test_file << "v7,v8,0.1           " << std::endl;
+        test_file << "v7,v9,0.1           " << std::endl;
+        test_file << "v8,v9,0.1           " << std::endl;
+        test_file.close();
+
+        g = uu::net::read_probabilistic_graph(test_file_name, "g", ',');
+
+    }
+
+    void
+    TearDown() override
+    {
+        std::remove(test_file_name.data());
+    }
+
+};
+
+
+TEST_F(pnet_measures_clustering_test, expected_clustering_coefficient)
+{
+    double x = 0;
+    auto v0 = g->vertices()->get("v0");
+    double c0 = uu::net::expected_clustering_coefficient(g.get(), v0);
+    EXPECT_EQ(x, c0)
+            << "wrong clustering coefficient, vertex v0";
+
+    x = 0.1;
+    auto v1 = g->vertices()->get("v1");
+    double c1 = uu::net::expected_clustering_coefficient(g.get(), v1);
+    EXPECT_EQ(x, c1)
+            << "wrong clustering coefficient, vertex v1";
+
+    x = 0.4/6;
+    auto v6 = g->vertices()->get("v6");
+    double c6 = uu::net::expected_clustering_coefficient(g.get(), v6);
+    EXPECT_EQ(x, c6)
+        << "wrong clustering coefficient, vertex v6";
+
+    //x = 0.05;
+    //auto v2 = g->vertices()->get("v2");
+    //double c2 = uu::net::expected_clustering_coefficient(g.get(), v2);
+    //EXPECT_EQ(x, c2)
+    //    << "wrong clustering coefficient, vertex v2";    
+}
+
