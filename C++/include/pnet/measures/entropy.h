3bf22a86c7c2efaf33e2f86157e96f6e0b9e1a3f
Amin <amin-kaveh@hotmail.com>
2018-08-28 11:58:39 +0200

    Graph Entropy is included

---
+++ C++/include/pnet/measures/entropy.h
@@ -0,0 +1,67 @@
+/**
+ * History:
+ * - 2018.08.16 file created.
+ */
+
+#ifndef UU_PNET_MEASURES_DEGREE_H_
+#define UU_PNET_MEASURES_DEGREE_H_
+
+#include<iostream>
+#include<complex>
+#include<vector>
+#include<cmath>
+#include "core/utils/Counter.h"
+
+#include "net/datastructures/objects/Vertex.h"
+#include "net/datastructures/objects/EdgeMode.h"
+
+namespace uu {
+namespace net {
+
+/**
+ * Returns the Entropy of graph.
+ * @param g input graph
+ * @return the Entropy of g
+ */
+template<typename G>
+double
+graph_entropy(
+               const G* g
+);
+
+
+/** DEFINITIONS */
+
+template<typename G>
+double
+graph_entropy(
+               const G* g
+)
+{
+
+
+    if (!g->is_probabilistic())
+    {
+        throw core::WrongParameterException("Entropy can only be computed on probabilistic graphs");
+    }
+
+    double E = 0;      
+    for (auto edge: *g->edges())
+    {
+        auto p = g->edges()->attr()->get_probability(edge);
+
+        if (!p.null)
+        {
+            E += -p.value*log10(p.value)-(1-p.value)*log10(1-p.value);
+        }
+        
+    }
+
+    return E;
+}
+
+
+}
+}
+
+#endif
---
+++ C++/test/pnet/measures/entropy_test.cpp
@@ -0,0 +1,82 @@
+#include "gtest/gtest.h"
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "pnet/io/read_probabilistic_graph.h"
+#include "pnet/measures/entropy.h"
+
+
+class pnet_measures_entropy_test : public ::testing::Test
+{
+  protected:
+
+    std::string test_file_name = "net_io_read_graph_file.tmp";
+    std::unique_ptr<uu::net::ProbabilisticGraph> g;
+
+    void
+    SetUp() override
+    {
+        // Create a test file
+        std::ofstream test_file;
+        test_file.open(test_file_name);
+
+        if (!test_file.is_open())
+        {
+            FAIL()
+                    << "Could not create temporary file. Test not executed.";
+        }
+
+        test_file << "#VERSION           " << std::endl;
+        test_file << "2.0                " << std::endl;
+        test_file << "                   " << std::endl;
+        test_file << "#TYPE              " << std::endl;
+        test_file << "undirected         " << std::endl;
+        test_file << "probabilistic      " << std::endl;
+        test_file << "                   " << std::endl;
+        test_file << "#VERTICES          " << std::endl;
+        test_file << "v0                 " << std::endl;
+        test_file << "                   " << std::endl;
+        test_file << "#EDGES             " << std::endl;
+        test_file << "v1,v2,0.1           " << std::endl;
+        test_file << "v1,v3,0.1           " << std::endl;
+        test_file << "v2,v3,0.1           " << std::endl;
+        test_file << "v2,v4,0.1           " << std::endl;
+        test_file << "v2,v5,0.1           " << std::endl;
+        test_file << "v2,v6,0.1           " << std::endl;
+        test_file << "v3,v5,0.1           " << std::endl;
+        test_file << "v3,v6,0.1           " << std::endl;
+        test_file << "v4,v5,0.1           " << std::endl;
+        test_file << "v5,v6,0.1           " << std::endl;
+        test_file << "v5,v7,0.1           " << std::endl;
+        test_file << "v6,v7,0.1           " << std::endl;
+        test_file << "v7,v8,0.1           " << std::endl;
+        test_file << "v7,v9,0.1           " << std::endl;
+        test_file << "v8,v9,0.1           " << std::endl;
+        test_file.close();
+
+        g = uu::net::read_probabilistic_graph(test_file_name, "g", ',');
+
+    }
+
+    void
+    TearDown() override
+    {
+        std::remove(test_file_name.data());
+    }
+
+};
+
+TEST_F(pnet_measures_entropy_test, graph_entropy)
+{
+    double e = uu::net::graph_entropy(g.get());
+    EXPECT_EQ(2.117726, e)
+            << "wrong entropy";
+
+   
+}
+
+
+
