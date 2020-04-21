e2ccf3902a2281919322310f8fa56607aa3a3ea5
xpopy <oskardahlin10@hotmail.com>
2020-04-21 14:12:06 +0200

    Updated eta_k_core to use indexes instead of names

--- C++/src/sparsification/eta_k_core.cpp
+++ C++/src/sparsification/eta_k_core.cpp
@@ -141,6 +141,7 @@ efficient_Poisson_Binomial_density_function
     return current_degree_distribution;
 }
 
+
 /**
  * (eta,k)-core
  * This function returns the (eta_k)-core value of all nodes in a probabilistic graph introduced in paper "Core Decomposition of Uncertain Graphs"
@@ -154,33 +155,31 @@ eta_k_core(
     double eta_threshold
 )
 {
-
-    // -------- !IMPORTANT! -------- 
-    // This function requires the 
-    // vertices in original_graph to be 
-    // in the format "v0", "v1" etc 
-    // -------- !IMPORTANT! --------
-    
     // initialize
     auto probGraph = duplicate_graph(original_graph);
     int n = maximum_degree(probGraph.get());
-    std::cout << "n: " << n << std::endl;
     int dim = probGraph->vertices()->size();
 
+    std::vector<std::string> stringAtIndex (dim);
+    std::unordered_map<std::string, int> indexAtString; 
     std::vector<double> core(dim);
     std::vector<int> ddev(dim);
     std::vector<std::vector<double>> D(n);
     std::vector<std::vector<double>> densities(dim);
 
-    std::cout << "starting 1 loop----------------" << std::endl;
+    for (auto vertex: *probGraph->vertices())
+    {
+        int index = probGraph->vertices()->index_of(vertex);
+        std::string name = vertex->to_string();
+        stringAtIndex[index] = name;
+        indexAtString[name] = index;
+    }
+
     // line 3 to 6
     for (auto vertex: *probGraph->vertices()){
        
-        std::cout << "\n" << vertex->to_string() << std::endl;
-
         auto len = probGraph->edges()->incident(vertex)->size();
-        int v_index = std::stoi(vertex->to_string().erase(0,1));
-        //int v_index = probGraph->vertices()->index_of(vertex);
+        int v_index = indexAtString[vertex->to_string()];
         std::vector<double> P;
         P.reserve(len);
         for (auto edge: *probGraph->edges()->incident(vertex))
@@ -191,58 +190,23 @@ eta_k_core(
         ddev[v_index] = density_distribution_eta_value(dist_v, eta_threshold);
         densities[v_index] = dist_v;
         D[ddev[v_index]].push_back(v_index);
-        
-
-
-        //  Debug prints --------------
-
-
-        std::cout << "dist_v: ";
-        for (auto el: dist_v){
-            std::cout << el << "  ";
-        }
-        std::cout << std::endl;
-        std::cout << "ddev[vertex]: ";
-        std::cout << ddev[v_index] << std::endl;
-
-        std::cout << "D:" << std::endl; 
-        for (auto el: D){
-            std::cout << " list: ";
-            for (auto el2: el){
-                std::cout << el2 << "  ";
-            }
-            std::cout << std::endl;
-        }
-        //Debug prints end -------------
     }
 
-    std::cout << "\n\n\nstarting 2nd loop----------------" << std::endl;
     // line 7 to 17
     for (int k = 0; k < n; k++)
     {
-        std::cout << "for iteration: " << k << std::endl;
         while (D[k].size() > 0)
         {
             int candidate_node = D[ k ].back();
-            std::cout << "using node: " << candidate_node << std::endl;
             D[k].pop_back();
             core[candidate_node] = k + 1;
-            std::cout << "D: " << std::endl;
-            for (auto el: D){
-                std::cout << " list: ";
-                for (auto el2: el){
-                    std::cout << el2 << "  ";
-                }
-                std::cout << std::endl;
-            }
 
             // line 11 to 15
-            auto rem_node = probGraph->vertices()->get("v" + std::to_string(candidate_node));
+            auto rem_node = probGraph->vertices()->get(stringAtIndex[candidate_node]);
             for(auto neighbor: *probGraph->edges()->neighbors(rem_node))
             {
                 std::cout << neighbor->to_string() << std::endl;
-                int neighbor_i = std::stoi(neighbor->to_string().erase(0,1));
-                std::cout << "ddev[neighbor_i] > k: " << (ddev[neighbor_i] > k + 1) << std::endl;
+                int neighbor_i = indexAtString[neighbor->to_string()];
                 if (ddev[neighbor_i] > k + 1 ) 
                 {
                     auto edge = probGraph->edges()->get(rem_node, neighbor);
@@ -255,31 +219,8 @@ eta_k_core(
                     // line 13
                     D[prev_deg_u].erase(std::remove(D[prev_deg_u].begin(), D[prev_deg_u].end(), neighbor_i), D[prev_deg_u].end());      
                     D[ddev[neighbor_i] ].push_back(neighbor_i);
-
-
-                    // -------------prints------------
-                    std::cout << "prev_deg_u: " << prev_deg_u << std::endl;
-                    std::cout << "ddev[neighbor_i]: " << ddev[neighbor_i] << std::endl;
-                    std::cout << "ddev: ";
-                    for (auto el: ddev){
-                        std::cout << el << "  ";
-                    }
-                    std::cout << std::endl;
-                    
-                    std::cout << "D neighbor: " << std::endl;
-                    for (auto el: D){
-                        std::cout << " list: ";
-                        for (auto el2: el){
-                            std::cout << el2 << "  ";
-                        }
-                        std::cout << std::endl;
-                    }
-
-                    // -------------prints end------------
-
                 }
             }
-            std::cout << "Removing vertex " << rem_node->to_string() << std::endl;
             probGraph->vertices()->erase(rem_node);
         }
     }
