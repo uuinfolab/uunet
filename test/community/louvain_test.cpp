302291068eaa4ded411ad0cf607f60e6d4434fa5
Matteo Magnani <matteo.magnani@it.uu.se>
2024-07-26 10:36:43 +0200

    added resolution parameter to louvain

--- test/community/louvain_test.cpp
+++ test/community/louvain_test.cpp
@@ -12,11 +12,57 @@ TEST_F(community_algo_test, louvain)
     }
 }
 
-TEST_F(community_algo_test, weighted_louvain)
+TEST_F(community_algo_test, louvain_weighted)
 {
 
     auto communities = uu::net::louvain(wnet.get());
     
     // Not a test of correctness
+    
+}
+
+TEST_F(community_algo_test, louvain_resolution)
+{
+
+    auto communities1 = uu::net::louvain(net.get(), 1.0);
+    
+    // Not a test of correctness
+    /*
+    std::cout << "Communities:" << std::endl;
+    for (auto com: *communities1)
+    {
+        for (auto v: *com)
+        {
+            std::cout << v->name << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    auto communities2 = uu::net::louvain(net.get(), .1);
+    
+    // Not a test of correctness
+    std::cout << "Communities:" << std::endl;
+    for (auto com: *communities2)
+    {
+        for (auto v: *com)
+        {
+            std::cout << v->name << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    auto communities3 = uu::net::louvain(net.get(), 10.0);
+    
+    // Not a test of correctness
+    std::cout << "Communities:" << std::endl;
+    for (auto com: *communities3)
+    {
+        for (auto v: *com)
+        {
+            std::cout << v->name << " ";
+        }
+        std::cout << std::endl;
+    }
+    */
     
 }
